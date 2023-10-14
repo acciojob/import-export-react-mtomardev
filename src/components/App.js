fb4695df2e497c9e0a3632db289188636657c3c6
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-10-14 19:21:23 +0000

    h

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
-import { Heading } from "./Heading";
+import Heading from "./Heading";
 import InputQuery from "./InputQuery";
 import SubHeading from "./SubHeading";
 import SubmitButton from "./SubmitButton";
